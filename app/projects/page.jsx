05f238e4af6b004a8562a83bba12f7f212c42e22
HYK <junhyeok001010@gmail.com>
2023-10-15 15:47:21 +0900

    .

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,7 +2,7 @@ import ProjectCard from '@/components/ProjectCard'
 import React from 'react'
 
 async function fetchProjects() {
-  const response = await fetch('http://localhost:3000/api/projects')
+  const response = await fetch('http://127.0.0.1:3000/api/projects')
   const projects = response.json()
   return projects
 }
@@ -11,7 +11,7 @@ export default async function page() {
   const projects = await fetchProjects()
 
   return (
-    <section className=" min-h-screen flex-col p-5">
+    <section className="min-h-screen flex-col p-5">
       <div className="container mx-auto grid gap-6 grid-cols-project-card">
         {projects?.map((project) => (
           <ProjectCard key={project.id} project={project} />
