a4be3ee77575ac6b4cca4af1e8d51c8adf072f7b
HYK <junhyeok001010@gmail.com>
2023-12-11 14:13:04 +0900

    .

--- app/projects/1/page.jsx
+++ app/projects/1/page.jsx
@@ -1,5 +1,7 @@
+'use client'
 import Markdown from 'react-markdown'
 import '@/app/markdown.css'
+import YouTube from 'react-youtube'
 
 export default function ProjectPage() {
   const document = `
@@ -8,11 +10,13 @@ export default function ProjectPage() {
   ### 신준혁 - GitHub 리포지터리 관리, 백엔드, 프론트엔드
   ### 김승언 - Community 페이지 제공
   ### 유혁준 - Leaderboard 페이지 제공
-  ### 박서진 - 프론트엔드`
+  ### 박서진 - 프론트엔드
+  ## 데모 영상`
   return (
     <div className="container mx-auto min-h-screen">
       <div className="mx-3 markdown-body">
         <Markdown>{document}</Markdown>
+        <YouTube videoId="RJ1ita6JrMo" />
       </div>
     </div>
   )
